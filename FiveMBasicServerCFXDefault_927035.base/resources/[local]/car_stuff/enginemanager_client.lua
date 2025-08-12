5ecbbc6a214be63418513b8dc70a451c175dd9a5
ParkerHansen7777 <parkerhansen7777@gmail.com>
2025-08-12 13:49:22 -0700

    Update enginemanager_client.lua

--- FiveMBasicServerCFXDefault_927035.base/resources/[local]/car_stuff/enginemanager_client.lua
+++ FiveMBasicServerCFXDefault_927035.base/resources/[local]/car_stuff/enginemanager_client.lua
@@ -3,37 +3,19 @@ AddEventHandler('playerSpawned', function()
 	-- get player's Ped ID
 	local playerPed = PlayerPedId()
 	print(playerPed)
+	
+	--create 'engine' command
 	RegisterCommand('engine', function()
 		local vehicle = GetVehiclePedIsUsing(playerPed, false)
 		if vehicle ~= 0 then
 			
 			-- toggle engine based on current state
 			SetVehicleEngineOn(vehicle, not GetIsVehicleEngineRunning(vehicle), false, true)
-			--print("toggled")`	
+				
 			
 		end
 	end , false)
-	RegisterKeyMapping('engine', 'engine on/off', 'keyboard', 'l')
-	--[[]
-	-- Constant checking if player is in a vehicle or not (every tick oof), temporary, needs to be optimized
-	while true do
-		
-		Wait(0)
-		-- Check if player is in vehicle
-		local vehicle = GetVehiclePedIsUsing(playerPed, false)
-		--print(vehicle)
-		
-		-- if L key is pressed
-		if(IsControlJustReleased(2, 182) and vehicle ~= 0) then
-			
-			-- toggle engine based on current state
-			SetVehicleEngineOn(vehicle, not GetIsVehicleEngineRunning(vehicle), false, true)
-			--print("toggled")`	
-			
-		end
-		
 	
-	end ]]
-		
-		
+	--default engine toggle to L key
+	RegisterKeyMapping('engine', 'engine on/off', 'keyboard', 'l')
 end)
